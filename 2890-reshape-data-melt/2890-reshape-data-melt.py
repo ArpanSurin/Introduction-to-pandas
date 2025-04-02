ed2b14520b380d322074cb094c05b96fd6d73436
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2025-04-02 22:09:05 +0530

    Time: 313 ms (87.86%), Space: 67.2 MB (5.93%) - LeetHub

---
+++ 2890-reshape-data-melt/2890-reshape-data-melt.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    report = report.melt(
+        id_vars = ['product'], 
+        var_name = 'quarter',
+        value_vars = ['quarter_1', 'quarter_2','quarter_3','quarter_4'], 
+        value_name = 'sales')
+    return report
